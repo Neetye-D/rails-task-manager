cffcdc3aad094d2f28c059d88c6c671eef221403
doorgesh neetye <neetyedoorgesh@gmail.com>
2021-07-28 11:13:17 +0400

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_080249) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_210_727_080_249) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.text 'details'
+    t.boolean 'completed', default: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
